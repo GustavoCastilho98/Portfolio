5671f033e738268f4a6003eb74829908a930b5b0
Gustavo Castilho <contatogrcastilho@gmail.com>
2020-03-29 10:40:46 -0300

    portfolio finalizado

--- index.js
+++ index.js
@@ -43,3 +43,13 @@ var typed_2 = new Typed ('#typed_2', {
     loop: true
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener('click',function(e){
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
+
